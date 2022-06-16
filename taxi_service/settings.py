fa03d60bcf569e6128f36bdfe7cc71de852fc7fb
nikon4uk <nikonchukmax4@gmail.com>
2022-06-16 12:12:04 +0300

    add heroku domain to ALLOWED_HOSTS

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-8ovil3xu6=eaoq
 # DEBUG = True
 DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
 
-ALLOWED_HOSTS = ["127.0.0.1", ]
+ALLOWED_HOSTS = ["127.0.0.1", "py-taxi-service.herokuapp.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
